cc13a9c6ff2ad0ac82f522c1ce38a2f92ae3c706
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2022-05-11 19:36:46 +0300

    Add script for cleaning repos of unsupported files

---
+++ hyperstyle/clean_repos.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+set_of_supported_extension = {".kt", ".kts", ".js", ".py", ".java"}
+
+
+def clean_repos(repos_dir):
+    for root, _, files in os.walk(repos_dir):
+        for file in files:
+            file_extension = os.path.splitext(file)[1]
+            if file_extension not in set_of_supported_extension:
+                os.remove(os.path.join(root, file))
+
+
+if __name__ == '__main__':
+    """
+    Clean repos of unsupported files.
+    1 argument expected - path to the folder with repos to be cleaned 
+    """
+    if len(sys.argv) != 1:
+        exit(1)
+
+    repos_dir = sys.argv[1]
+    clean_repos(repos_dir)
+    print("Now repos are clean!")
